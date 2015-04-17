61e19a8f43d092472970132ceb10425104b28910
Daniels Pitkevičs <daniels.pitkevics@gmail.com>
2015-04-17 11:59:52 +0300

    Jooglin-20 Set as default search engine and set as homepage buttons

--- Help/urls.py
+++ Help/urls.py
@@ -4,4 +4,6 @@ from Help import views
 
 urlpatterns = patterns('',
                        url(r'^levels/$', views.user_levels, name='user_levels'),
+                       url(r'^homepage/$', views.set_as_homepage, name='set_as_homepage'),
+                       url(r'^search-engine/$', views.set_as_default_search_engine, name='set_as_default_search'),
                        )
--- Help/views.py
+++ Help/views.py
@@ -5,3 +5,15 @@ def user_levels(request):
     context = {}
 
     return render(request, 'Help/user_levels.html', context)
+
+
+def set_as_homepage(request):
+    context = {}
+
+    return render(request, 'Help/set_as_homepage.html', context)
+
+
+def set_as_default_search_engine(request):
+    context = {}
+
+    return render(request, 'Help/set_as_default_search_engine.html', context)
